97f2c85a04487caea881c1fc6adc334a33be895a
Wolf117Warrior <wolf117warrior@openmailbox.org>
2016-04-24 18:40:12 +0200

    maj

--- include/footer.php
+++ include/footer.php
@@ -2,7 +2,7 @@
             <!-- Pied de la page -->
             <footer class="wrapper">
                         <a href="./mentions.php">Mention légales</a></li>
-                        <h3>Les membres du mini projet > Émilien et Tiphaine</h3><h3><a href="./apropos.php">Édito</a></h3>
+                        <h3>Les membres du mini projet - Émilien et Tiphaine</h3><h3><a href="./apropos.php">Édito</a></h3>
                     </ul>
                 </nav>
             </footer>

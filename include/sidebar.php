0f75f3eb7dbbe5d674da44e4a3e8eca033e73568
Wolf117Warrior <wolf117warrior@openmailbox.org>
2016-05-01 20:02:54 +0200

    Modif
    Modif

--- include/sidebar.php
+++ include/sidebar.php
@@ -1,5 +1,5 @@
  <!-- Bandeau de la page -->
                 <aside>
                     <h3>Annonces</h3>
-                    <p>Le site est actuellement en construction, je vous invite à vous rendre sur <a href="http://sissi-forumofficiel.fr">le forum officiel français</a>, qui est également en développement dans un stade très avancer.</p>
+                    <p>Le site est actuellement en construction, je vous invite à vous rendre sur <a href="http://sissi-forumofficiel.fr">le forum officiel français</a>, car celui-ci n'est autre qu'un site de projet/test.</p>
                 </aside>

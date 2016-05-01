4c145fb64d1e6770dbd5a6e13f82e40bbcb8ee53
Wolf117Warrior <wolf117warrior@openmailbox.org>
2016-05-01 21:47:14 +0200

    MaJ
    Mise en place du logo et de la favicon

--- include/header.php
+++ include/header.php
@@ -1,6 +1,7 @@
 <div class="header-container">
             <header class="wrapper clearfix">
                 <!-- Entete de la page -->
+                <img src="./img/logo.png" style="width: 109px;height: 75px;float: left;">
                 <h1 class="title">Blog de Sissi</h1>
                 <nav>
                     <ul>

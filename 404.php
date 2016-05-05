78de8ab9968442101ba24a5ec522be61d824855e
Wolf117Warrior <wolf117warrior@openmailbox.org>
2016-05-05 13:35:36 +0200

    Mise  a jour
    Modif de la page 404

---
+++ 404.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html class="no-js" lang="fr">
+    <!-- Entete HTML -->
+    <?php include("./include/head.php"); ?>
+
+    <body>
+        <!-- Menu de la page -->
+        <?php include("./include/header.php"); ?>
+
+        <div class="main-container">
+            <div class="main wrapper clearfix">
+                <!-- Corps de la page -->
+                <article>
+                  <h1>Page Not Found</h1>
+                  <p>Sorry, but the page you were trying to view does not exist.</p>
+                </article>
+
+                <!-- Bandeau de la page -->
+               <?php include("./include/sidebar.php"); ?>
+
+           </div> <!-- #main -->
+       </div> <!-- #main-container -->
+
+       <!-- Pieds de la page -->
+       <?php include("./include/footer.php"); ?>
+
+       <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+       <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+       <script src="js/main.js"></script>
+   </body>
+</html>

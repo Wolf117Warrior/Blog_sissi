601ee256cf9b5c8d65ae6f98fa8cfc689ecce6fc
Wolf117Warrior <wolf117warrior@openmailbox.org>
2016-05-16 14:43:11 +0200

    Diverse MaJ

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 <html class="no-js" lang="fr">
     <!-- Entete HTML -->
     <?php include("./include/head.php"); ?>
-    
+
     <body>
         <!-- Menu de la page -->
         <?php include("./include/header.php"); ?>
@@ -11,21 +11,23 @@
             <div class="main wrapper clearfix">
                 <!-- Corps de la page -->
                 <article>
-                	
-                 <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1> 
-                 
-                 <h2>Vestibulum sit amet vulputate erat. Etiam non scelerisque nulla, ac scelerisque ipsum.</h2> 
-                 
-                 <p>Phasellus scelerisque risus tortor, sit amet scelerisque nisi fermentum id. Praesent tristique urna eu lacus auctor lacinia. Nullam quis arcu dapibus, fringilla neque a, consectetur turpis. Nam aliquam tempus magna. Quisque sed interdum tortor, vitae tincidunt massa. Praesent nunc nibh, tincidunt vel vehicula ac, scelerisque eget tellus. Curabitur eget euismod ligula, non sollicitudin nibh. Suspendisse vitae suscipit sem. Integer pharetra diam at scelerisque faucibus. Integer ac sollicitudin tortor. Suspendisse hendrerit justo elit, vitae tempor libero bibendum quis. Nulla scelerisque nisi eu est lobortis rutrum ac nec felis. Sed fermentum nibh at est aliquam fermentum.</p>
-               
-                 <h2>Phasellus maximus mi nec libero rutrum, quis efficitur purus luctus. </h2>
-                 
-                 <p>Fusce quis neque nec elit iaculis varius. Quisque rutrum, ligula non ullamcorper aliquet, est ante dictum arcu, eu pharetra mi mi at orci. Cras imperdiet malesuada urna, ut feugiat magna dignissim in. Sed ullamcorper elit magna, vel condimentum lacus malesuada nec. Aliquam non nulla enim. Cras euismod ex erat, et tempus mauris semper sed. Integer ut augue at nisi iaculis egestas. Integer sit amet libero ac dui elementum viverra a quis risus. Integer tincidunt turpis sed purus posuere, vel suscipit magna ornare. In et porta ex. Etiam vitae nisl faucibus, maximus tellus sit amet, malesuada dolor. Sed commodo ex eget viverra viverra.</p>
-                	
-                 <h2>Duis ut lorem sollicitudin massa sodales gravida eu finibus dui.</h2>
-                 
-                 <p>Nullam cursus, nulla eu ultricies accumsan, velit magna egestas ex, vel eleifend enim est eu magna. Donec molestie malesuada augue at finibus. Nunc consectetur vitae velit sit amet mattis. Sed eget diam a ex consectetur posuere. Ut euismod faucibus velit, ac ultricies ante pharetra ac. Vivamus et libero vitae dolor scelerisque cursus. Fusce in ligula faucibus, ultricies orci sit amet, fermentum odio. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
-               
+
+                  <h3>Remplissez ce formulaire pour pouvoir nous contacter.</h3>
+                  <form method="post" action="mailto:emilien.locatelli@tech-blog.fr" enctype="text/plain">
+
+                    Nom et prénom : <input type="text" name="nom" maxlength="30"/><br />
+                      <input type="radio" name="situation" id="situation" value="1" cheked><br>
+                      <input type="radio" name="situation" value="2"><br>
+                      <input type="radio" name="situation" value="3"><br>
+
+
+
+                    <input type="submit" value="OK" />
+
+
+                  </form>
+
+                </article>
             </div> <!-- #main -->
         </div> <!-- #main-container -->
 
